30503da031e62b099a168565ffa325e227eddfb5
luskanek <babecka@outlook.com>
2021-12-02 06:51:09 +0100

    pridaný kód pre nahrávanie obrázkov

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function showHamburger() 
-{
+function showHamburger()  {
     var x = document.getElementById("navbar");
     var y = document.getElementById("hamburger-menu");
 
@@ -21,4 +20,49 @@ function showNavbar() {
     if (x.style.display === "none") {
         x.style.display = "block";
     }
+}
+
+function hideCategories() {
+    var x = document.getElementById("categories");
+    var y = document.getElementById("items");
+
+    x.style.display = "none";
+    y.style.display = "block";
+}
+
+window.onload = function() {
+    var wrapper = document.getElementById("wrapper-images");
+    var input = document.getElementById("input-new-images");
+
+    input.addEventListener("change", function(e) {
+        if (e.target.files.length > 12) {
+            alert("Môžete nahrať maximálne 12 obrázkov!");
+        }
+        else {
+        var images = wrapper.getElementsByTagName("img");
+
+        while (images.length > 0) {
+            wrapper.removeChild(images[0]);
+        }
+        
+        var files = e.target.files;
+
+        for (let i = 0; i < files.length; i++) {
+            var file = files[i];
+
+            var Reader = new FileReader();
+            Reader.addEventListener("load", function(e) {
+                var image = document.createElement("img");
+
+                image.src = e.target.result;
+                image.setAttribute("width", "100px");
+                image.setAttribute("height", "100px");
+
+                wrapper.appendChild(image);
+            });
+
+            Reader.readAsDataURL(file);
+        }
+    }
+    });
 }
