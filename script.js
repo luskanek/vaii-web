9dbbb0836777cb55d397de8176360b12d124b1ae
luskanek <babecka@outlook.com>
2021-12-02 10:56:22 +0100

    validácia nahrávaných súborov

--- script.js
+++ script.js
@@ -30,39 +30,63 @@ function hideCategories() {
     y.style.display = "block";
 }
 
+function displayMessage(text) {
+    var x = document.getElementById("info-message");
+
+    x.innerHTML = text;
+    x.style.display = "block";
+}
+
 window.onload = function() {
     var wrapper = document.getElementById("wrapper-images");
     var input = document.getElementById("input-new-images");
+    var x = document.getElementById("info-message");
+    var extensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
 
     input.addEventListener("change", function(e) {
-        if (e.target.files.length > 12) {
-            alert("Môžete nahrať maximálne 12 obrázkov!");
-        }
-        else {
+        x.style.display = "none";
         var images = wrapper.getElementsByTagName("img");
 
         while (images.length > 0) {
             wrapper.removeChild(images[0]);
         }
-        
-        var files = e.target.files;
 
-        for (let i = 0; i < files.length; i++) {
-            var file = files[i];
-
-            var Reader = new FileReader();
-            Reader.addEventListener("load", function(e) {
-                var image = document.createElement("img");
-
-                image.src = e.target.result;
-                image.setAttribute("width", "100px");
-                image.setAttribute("height", "100px");
-
-                wrapper.appendChild(image);
-            });
-
-            Reader.readAsDataURL(file);
+        if (e.target.files.length > 10) {
+            displayMessage("Môžete nahrať maximálne 10 obrázkov!");
+            input.value = "";
         }
-    }
+        else {
+            var uploads = e.target.files;
+
+            for (let i = 0; i < uploads.length; i++) {
+                if (!extensions.exec(input.value)) {
+                    displayMessage("Môžete nahrať len obrázky!");
+                    input.value = "";
+                    return false;
+                }
+                
+                if ((input.files.item(i).size / 1024) > 8192) {
+                    displayMessage("Môžete nahrať len obrázky s veľkosťou do 8MB!");
+                    input.value = "";
+                    return false;
+                }
+                
+                var file = uploads[i];
+
+                var Reader = new FileReader();
+                Reader.addEventListener("load", function(e) {
+                    var image = document.createElement("img");
+
+                    image.src = e.target.result;
+                    image.setAttribute("width", "100px");
+                    image.setAttribute("height", "100px");
+
+                    wrapper.appendChild(image);
+                });
+
+                Reader.readAsDataURL(file);
+            }
+            return true;
+        }
     });
 }
