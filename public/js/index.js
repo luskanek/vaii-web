546346314e8784019953086a8ea0501e1f79f6e3
luskanek <babecka@outlook.com>
2022-01-17 15:51:09 +0100

    placeholder pre prázdnu kategóriu

--- public/js/index.js
+++ public/js/index.js
@@ -30,22 +30,27 @@ function getItems(category) {
         url: "?c=home&a=getItemsInCategory&p=" + category,
         dataType: "json",
         success: function(response) {
-            for (let i = 0; i < response.length; i++) {
-                let item = response[i];
+            if (response.length == 0) {
+                let html = "<div id='empty-category'><i class='fas fa-box-open'></i><h3>Táto kategória neobsahuje inzeráty..</h3></div>";
+                $("#items").append(html);
+            } else {
+                for (let i = 0; i < response.length; i++) {
+                    let item = response[i];
 
-                $.ajax({
-                    type: "GET",
-                    url: "?c=user&a=getUserName&p=" + item.author,
-                    dataType: "json",
-                    async: false,
-                    success: function(tmp_response) {
-                        item.author = tmp_response;
-                    }
-                });
+                    $.ajax({
+                        type: "GET",
+                        url: "?c=user&a=getUserName&p=" + item.author,
+                        dataType: "json",
+                        async: false,
+                        success: function(tmp_response) {
+                            item.author = tmp_response;
+                        }
+                    });
 
-                let img = item.files.split(";");
-                let div = "<div class='item'><div class='item-image' style='background-image: url(&quot;uploads/" + img[0] + "&quot;)'></div><h3 class='title'>" + item.title + "</h3><p class='author'>" + item.author + "</p><p class='desc'>" + item.description + "</p><p class='price'>" + item.price + "€</p></div>";
-                $("#items").append(div);
+                    let img = item.files.split(";");
+                    let html = "<div class='item'><div class='item-image' style='background-image: url(&quot;uploads/" + img[0] + "&quot;)'></div><h3 class='title'>" + item.title + "</h3><p class='author'>" + item.author + "</p><p class='desc'>" + item.description + "</p><p class='price'>" + item.price + "€</p></div>";
+                    $("#items").append(html);
+                }
             }
         }
     });
