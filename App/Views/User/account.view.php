27c63d82f907801a2a8e96671e5d9576704ec8a2
luskanek <babecka@outlook.com>
2022-01-17 23:52:11 +0100

    drobné úpravy použivateľa

--- App/Views/User/account.view.php
+++ App/Views/User/account.view.php
@@ -15,7 +15,7 @@
         <p><?= $data->username ?></p>
         <h3>Telefónne číslo</h3>
         <p id="user-phone"><?= $data->phone ?><i id="user-phone-update" class="fas fa-edit" onclick="updatePhone()" style="margin-left: 5px; cursor: pointer;"></i></p>
-        <input id="user-input-phone" type="tel">
+        <input id="user-input-phone" type="tel" pattern="[0-9]{4} [0-9]{3} [0-9]{3}" required>
         <i id="user-phone-confirm" class="fas fa-check" onclick="updatePhone()" style="margin-left: 5px; cursor: pointer; display: none;"></i>
     </div>
 
